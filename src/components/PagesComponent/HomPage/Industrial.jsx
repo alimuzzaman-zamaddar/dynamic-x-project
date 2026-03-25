3c2c06c13345337ec74af93e3425bbaea6835bab
fahim-sarker <sarkerfahim599@gmail.com>
2026-03-25 16:29:45 +0600

    feat:complete the dime section

--- src/components/PagesComponent/HomPage/Industrial.jsx
+++ src/components/PagesComponent/HomPage/Industrial.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NewBanner from '../../CommonComponents/NewBanner'
+import Dime from "../../PagesComponent/industrial/Dime"
 import InduatrialBg from "../../../assets/img/industrial/industrialbg.png"
 
 const Industrial = () => {
   return (
     <section>
       <NewBanner image={InduatrialBg} title={"DIME & COMPONENTI INDUSTRIALI"} />
+      <Dime/>
     </section>
   )
 }
