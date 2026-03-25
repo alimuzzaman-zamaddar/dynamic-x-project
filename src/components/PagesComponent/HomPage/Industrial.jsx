fe0b71c47d071fe07266b8e4fb2adbd9e871365f
fahim-sarker <sarkerfahim599@gmail.com>
2026-03-25 16:50:13 +0600

    feat:complete the materiali section

--- src/components/PagesComponent/HomPage/Industrial.jsx
+++ src/components/PagesComponent/HomPage/Industrial.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import NewBanner from '../../CommonComponents/NewBanner'
 import Dime from "../../PagesComponent/industrial/Dime"
+import Materiali from "../../PagesComponent/industrial/Materiali"
 import InduatrialBg from "../../../assets/img/industrial/industrialbg.png"
 
 const Industrial = () => {
   return (
     <section>
       <NewBanner image={InduatrialBg} title={"DIME & COMPONENTI INDUSTRIALI"} />
-      <Dime/>
+      <Dime />
+      <Materiali/>
     </section>
   )
 }
