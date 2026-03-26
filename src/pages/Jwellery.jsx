7467fe0f22263df12c697bbef4ec3c307378a66b
fahim-sarker <sarkerfahim599@gmail.com>
2026-03-26 11:42:08 +0600

    feat:complete the micro section

--- src/pages/Jwellery.jsx
+++ src/pages/Jwellery.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Jwellerybanner from "../assets/img/bg/jwellery.png"
 import NewBanner from '../components/CommonComponents/NewBanner'
+import Micro from '../components/PagesComponent/Jwellery/Micro'
 
 const Jwellery = () => {
   return (
     <>
       <NewBanner image={Jwellerybanner} />
+      <Micro />
     </>
   )
 }
