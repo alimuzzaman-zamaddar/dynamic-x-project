36151ef56bc4a144b9b132f92bcc813abfc8ae3a
fahim-sarker <sarkerfahim599@gmail.com>
2026-03-26 11:48:52 +0600

    feat:complete the bello section

--- src/pages/Jwellery.jsx
+++ src/pages/Jwellery.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Jwellerybanner from "../assets/img/bg/jwellery.png"
 import NewBanner from '../components/CommonComponents/NewBanner'
 import Micro from '../components/PagesComponent/Jwellery/Micro'
+import Bello from '../components/PagesComponent/Jwellery/Bello'
 
 const Jwellery = () => {
   return (
     <>
       <NewBanner image={Jwellerybanner} />
       <Micro />
+      <Bello />
     </>
   )
 }
