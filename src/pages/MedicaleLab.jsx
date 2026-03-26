b89d2661bcc18dec4e513f9b4adfb225b3518477
fahim-sarker <sarkerfahim599@gmail.com>
2026-03-26 10:30:08 +0600

    feat:complete the scientifica section

--- src/pages/MedicaleLab.jsx
+++ src/pages/MedicaleLab.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NewBanner from '../components/CommonComponents/NewBanner'
 import MedicaleBanner from "../assets/img/bg/medicalebanner.png"
+import Scientifica from '../components/PagesComponent/Medical/Scientifica'
 
 const MedicaleLab = () => {
   return (
     <>
       <NewBanner image={MedicaleBanner} title={"MEDICALE, LAB & BIOTECH"} />
+      <Scientifica />
     </>
   )
 }
