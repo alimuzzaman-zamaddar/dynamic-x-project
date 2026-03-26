f7a98cdd033fcaa0611160af8b250914e5bd07da
fahim-sarker <sarkerfahim599@gmail.com>
2026-03-26 10:14:04 +0600

    feat:complete the industrial section

--- src/components/PagesComponent/industrial/Pronto.jsx
+++ src/components/PagesComponent/industrial/Pronto.jsx
@@ -4,7 +4,7 @@ import { Car, Clock, Usd } from '../../SvgContainer/SvgContainer'
 
 const Pronto = () => {
   return (
-    <section className='lg:py-18 py-8'>
+    <section className='lg:pt-18 pt-8'>
       <Container>
         <h2 className='lg:text-4xl text-2xl font-semibold text-[#0A0A0A]'>Pronto a Stampare il Tuo Progetto?</h2>
         <p className='text-xs font-normal text-[#1E2939] pt-4'>Carica il tuo file o contattaci per una consulenza tecnica gratuita. Analizziamo la tua geometria e ti proponiamo il materiale e la tecnologia più adatta.</p>
