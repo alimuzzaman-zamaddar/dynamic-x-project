0dbb639e076c7762f45ce79b54dee1598bf3bfbc
fahim-sarker <sarkerfahim599@gmail.com>
2026-03-26 10:50:08 +0600

    feat:complete the funzioable section

--- src/pages/MedicaleLab.jsx
+++ src/pages/MedicaleLab.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import NewBanner from '../components/CommonComponents/NewBanner'
 import MedicaleBanner from "../assets/img/bg/medicalebanner.png"
 import Scientifica from '../components/PagesComponent/Medical/Scientifica'
+import Funzionable from '../components/PagesComponent/Medical/Funzionable'
 
 const MedicaleLab = () => {
   return (
     <>
       <NewBanner image={MedicaleBanner} title={"MEDICALE, LAB & BIOTECH"} />
       <Scientifica />
+      <Funzionable />
     </>
   )
 }
