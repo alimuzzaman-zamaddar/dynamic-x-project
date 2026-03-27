50bdb519cb149a3295cc35f5af6669e467cce9be
fahim-sarker <sarkerfahim599@gmail.com>
2026-03-27 12:02:44 +0600

    feat:complete the vita section

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Newbanner from "../components/CommonComponents/NewBanner"
 import CatalogBanner from "../assets/img/bg/catalogbanner.png"
 import Futuro from '../components/PagesComponent/Catalog/Futuro'
+import Vita from '../components/PagesComponent/Catalog/Vita'
 
 const Catalog = () => {
   return (
     <>
       <Newbanner title={"Catalogo"} image={CatalogBanner} showButton={false} />
       <Futuro />
+      <Vita />
     </>
   )
 }
