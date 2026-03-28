9a72e6e3a8b12030ffb2b7a0fb9bc3fadac51183
fahim-sarker <sarkerfahim599@gmail.com>
2026-03-28 11:44:17 +0600

    feat: added link for fashion page all button

--- src/components/PagesComponent/Fashion/Visione.jsx
+++ src/components/PagesComponent/Fashion/Visione.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Container from '../../../shared/Container'
+import { Link } from 'react-router'
 
 const Visione = () => {
   return (
@@ -11,20 +12,24 @@ const Visione = () => {
           <div className="rounded-sm border border-[#D1D5DC] p-6 flex flex-col items-center">
             <h4 className='text-[#0A0A0A] text-base text-center font-bold'>Consulenza Creativa</h4>
             <h5 className='text-xs font-normal text-center text-[#1E2939] py-4'>Discutiamo le tue ambizioni sartoriali. Un nostro specialista analizzerà la fattibilità del tuo progetto entro 24 ore.</h5>
-            <button
-              className='px-7 py-3 rounded-4xl bg-[#1A1A2E] cursor-pointer text-white hover:bg-transparent hover:text-black border border-[#1A1A2E] duration-300 ease-in-out text-base font-semibold'
-            >
-              Richiedi Consulenza
-            </button>
+            <Link to={"/upload-design"}>
+              <button
+                className='px-7 py-3 rounded-4xl bg-[#1A1A2E] cursor-pointer text-white hover:bg-transparent hover:text-black border border-[#1A1A2E] duration-300 ease-in-out text-base font-semibold'
+              >
+                Richiedi Consulenza
+              </button>
+            </Link>
           </div>
           <div className="rounded-sm border border-[#D1D5DC] p-6 flex flex-col items-center">
             <h4 className='text-[#0A0A0A] text-base text-center font-bold'>Inizia la Produzione</h4>
             <h5 className='text-xs font-normal text-center text-[#1E2939] py-4'>Condividi il tuo file 3D e scopri come possiamo dare forma al tuo design esclusivo con precisione sartoriale.</h5>
-            <button
-              className='px-7 py-3 rounded-4xl bg-[#1A1A2E] cursor-pointer text-white hover:bg-transparent hover:text-black border border-[#1A1A2E] duration-300 ease-in-out text-base font-semibold'
-            >
-              Carica il Progetto
-            </button>
+            <Link to={"/upload-design"}>
+              <button
+                className='px-7 py-3 rounded-4xl bg-[#1A1A2E] cursor-pointer text-white hover:bg-transparent hover:text-black border border-[#1A1A2E] duration-300 ease-in-out text-base font-semibold'
+              >
+                Carica il Progetto
+              </button>
+            </Link>
           </div>
         </div>
       </Container>
