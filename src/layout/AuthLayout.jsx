dd00178d62870b624f7cbd7ef37c52cf8ca15a8d
towfiq-islam <tofiqislam03@gmail.com>
2026-05-21 15:51:33 +0600

    modified auth layout

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,7 +1,25 @@
-import { Outlet } from "react-router";
-import authBg from "../assets/img/login.jpg";
+import { Outlet, useLocation } from "react-router";
+import loginBg from "../assets/img/login.jpg";
+import registerBg from "../assets/img/register.jpg";
+import otpBg from "../assets/img/otp.jpg";
+import emailBg from "../assets/img/forgot.jpg";
+import resetBg from "../assets/img/reset.jpg";
+import successBg from "../assets/img/success.jpg";
 
 const AuthLayout = () => {
+  const location = useLocation();
+
+  const authImages = {
+    "/auth/login": loginBg,
+    "/auth/register": registerBg,
+    "/auth/verify-otp": otpBg,
+    "/auth/verify-email": emailBg,
+    "/auth/reset-password": resetBg,
+    "/auth/success-message": successBg,
+  };
+
+  const authBg = authImages[location.pathname];
+
   return (
     <main className="min-h-screen flex items-center justify-center bg-white">
       <div className="px-3 md:px-10 xl:px-0 max-w-6xl 2xl:max-w-7xl mx-auto my-5 xl:my-10 flex items-center gap-8 xl:gap-10 w-full">
