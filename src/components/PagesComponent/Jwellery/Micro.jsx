5909f8b5dd1001653c3832af1a815532c69b017f
fahim-sarker <sarkerfahim599@gmail.com>
2026-06-05 15:01:43 +0600

    feat: intriget the micro  section cms api

--- src/components/PagesComponent/Jwellery/Micro.jsx
+++ src/components/PagesComponent/Jwellery/Micro.jsx
@@ -1,41 +1,35 @@
 import React from "react";
 import Container from "../../../shared/Container";
 
-const Micro = () => {
+const Micro = ({ data }) => {
   return (
     <section className="lg:py-18 py-8">
       <Container>
         <h2 className="lg:text-4xl text-2xl font-semibold text-[#0A0A0A]">
-          Micro-Fusione: L'Arte dell'Eccellenza Orefice
+          {data?.title}
         </h2>
         <p className="text-[15px] font-normal text-[#1E2939] pt-4">
-          Dove la precisione assoluta diviene pura poesia. Eleviamo il concetto
-          di manifattura luxury attraverso una risoluzione d'avanguardia,
-          cesellando ogni singolo dettaglio per trasformare le vostre visioni in
-          capolavori di ineguagliabile perfezione sartoriale.
+          {data?.description}
         </p>
+
         <div className="flex md:flex-row flex-col mt-10 border border-[#D1D5DC]">
-          <div className="w-full">
-            <div className="py-4 px-6 border-r  border-[#D1D5DC]">
-              <h4 className="text-[#0A0A0A] text-[14px] font-bold text-center">
-                Alta Gioielleria
-              </h4>
-            </div>
-          </div>
-          <div className="w-full">
-            <div className="py-4 px-6 border-r  border-[#D1D5DC]">
-              <h4 className="text-[#0A0A0A] text-[14px] font-bold text-center">
-                Micro-Dettaglio Sublime
-              </h4>
-            </div>
-          </div>
-          <div className="w-full">
-            <div className="py-4 px-6 border-r  border-[#D1D5DC]">
-              <h4 className="text-[#0A0A0A] text-[14px] font-bold text-center">
-                Fusione a Cera Persa
-              </h4>
-            </div>
-          </div>
+          {data?.divs?.map((item, index) => {
+            const isLast = index === data.divs.length - 1;
+
+            return (
+              <div key={index} className="w-full">
+                <div
+                  className={`py-4 px-6 text-center 
+                    ${!isLast ? 'md:border-r border-b md:border-b-0 border-[#D1D5DC]' : ''}
+                  `}
+                >
+                  <h4 className="text-[#0A0A0A] text-[14px] font-bold">
+                    {item.title}
+                  </h4>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </Container>
     </section>
--- src/pages/Jwellery.jsx
+++ src/pages/Jwellery.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Jwellerybanner from "../assets/img/bg/jwellery.png"
 import Contact from '../components/CommonComponents/Contact'
 import Micro from '../components/PagesComponent/Jwellery/Micro'
@@ -9,18 +9,54 @@ import Metallo from '../components/PagesComponent/Jwellery/Metallo'
 import Qualita from '../components/PagesComponent/Jwellery/Qualita'
 import Iniziate from '../components/PagesComponent/Jwellery/Iniziate'
 import Creazione from '../components/PagesComponent/Jwellery/Creazione'
+import { PageLoader } from '../shared/Loader';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
+
 
 const Jwellery = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}/cms/jwellery`);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const NewBannerData = data?.data?.hero || {};
+  const MicroData = data?.data?.micro_fusione_larte_delleccellenza_orefice || {};
+  const BelloData = data?.data?.la_tecnologia_al_servizio_del_bello || {};
+  const CreazioneData = data?.data?.la_tecnologia_al_servizio_del_bello || {};
+  const MetalloData = data?.data?.una_produzione_per_ogni_creazione || {};
+  const QualitaData = data?.data?.processo_dalla_visione_al_metallo || {};
+  const LussoData = data?.data?.partner_ideale_per_il_lusso || {};
+  const IniziateData = data?.data?.iniziate_a_collaborare_con_noi || {};
+
+
+
+  if (loading) {
+    return <PageLoader />;
+  }
+
   return (
     <>
-      <NewBanner image={Jwellerybanner} />
-      <Micro />
-      <Bello />
-      <Creazione />
-      <Metallo />
-      <Qualita />
-      <Lusso />
-      <Iniziate />
+      <NewBanner image={NewBannerData.image || Jwellerybanner} />
+      <Micro data={MicroData} />
+      <Bello data={BelloData} />
+      <Creazione data={CreazioneData} />
+      <Metallo data={MetalloData} />
+      <Qualita data={QualitaData} />
+      <Lusso data={LussoData} />
+      <Iniziate data={Iniziate} />
       <div className="lg:pb-18 pb-8">
         <Contact />
       </div>
